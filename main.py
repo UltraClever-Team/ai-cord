e4c97da1abdee29735baac008e23d88e7b66d9fb
An <quyenan2k11@gmail.com>
2026-02-08 10:16:17 +0000

    Implement Discord bot functionality with AI response capability and add knowledge file

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
-# import discord
-# from discord.ext import commands
+import discord
 import os
 from dotenv import load_dotenv
 load_dotenv()
 from google import genai
+from google.genai import types
+
+knowledge = open("knowledge.txt", "r").read()
 
 token = os.getenv('DISCORD_TOKEN')
 
 client = genai.Client()
 
-response = client.models.generate_content(
-    model="gemini-3-flash-preview", contents="Explain how AI works in a few words"
-)
-print(response.text)
+chat = client.chats.create(model="gemini-3-flash-preview", config=types.GenerateContentConfig(system_instruction=f"{knowledge}"))
+def ask(prompt):
+    response = chat.send_message(prompt)
+    return response.text
+
+bot = discord.Client(intents=discord.Intents.default())
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} working!')
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    if bot.user.mentioned_in(message):
+        prompt = message.content.replace(f'<@{bot.user.id}>', '').strip()
+        reply = ask(prompt)
+        await message.channel.send(reply)
+
+bot.run(token)
